87be753785a0cc238df7eff1d487eac8ca40ff1b
Richi-9427 <madanalarichitha@gmail.com>
2023-12-17 13:28:54 +0530

    bajaj

---
+++ Bajaj.java
@@ -0,0 +1,18 @@
+package com.jsp.application;
+
+public class Bajaj implements Bike
+{
+
+	@Override
+	public String bikeName() {
+		// TODO Auto-generated method stub
+		return "pulser";
+	}
+
+	@Override
+	public String cc() {
+		// TODO Auto-generated method stub
+		return "220cc";
+	}
+
+}
